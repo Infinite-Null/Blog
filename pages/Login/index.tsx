68029f87b2de0fdc7c0597c897dbad9a70ad171a
Infinite-Null <ankit.kum.sha9933@gmail.com>
2023-07-21 20:29:44 +0530

    Created login page

---
+++ pages/Login/index.tsx
@@ -0,0 +1,82 @@
+import { Card,  Input, Text, Button, Row , Spacer} from "@nextui-org/react";
+import { useState } from 'react';
+export default function App(){
+    const [login,setlogin]=useState(true)
+    function change(a:boolean){
+        setlogin(()=>a)
+    }
+    return <div style={{display:"flex",height:"80vh",alignItems:"center",justifyContent:"center"}}>
+        {(login==true)?<Login change={change}/>:
+        <Signup change={change}/>}
+    </div>
+}
+
+function Login({change}:{change:(a:boolean)=>void}){
+    return <Card css={{ mw: "330px", border:"2px solid black"}} isHoverable>
+    <Card.Header>
+      <Text b>Login</Text>
+    </Card.Header>
+    <Card.Body css={{ py: "$10" }}>
+    <Input 
+    underlined 
+    labelLeft="Email" 
+    placeholder="@gmail.com" 
+  />
+  <Spacer/>
+  <Input 
+    underlined 
+    labelLeft="Password" 
+    placeholder="****" 
+  />
+    </Card.Body>
+    <Card.Divider />
+    <Card.Footer>
+      <Row justify="flex-end">
+        <Button size="sm"  light onClick={()=>{
+            change(false)
+        }}>
+          Signup
+        </Button>
+        <Button size="sm" color="secondary">Login</Button>
+      </Row>
+    </Card.Footer>
+  </Card>
+}
+
+function Signup({change}:{change:(a:boolean)=>void}){
+    return <Card css={{ mw: "330px", border:"2px solid black"}} isHoverable>
+    <Card.Header>
+      <Text b>Signup</Text>
+    </Card.Header>
+    <Card.Body css={{ py: "$10" }}>
+    <Input 
+    underlined 
+    labelLeft="Name" 
+    placeholder="" 
+  />
+  <Spacer/>
+    <Input 
+    underlined 
+    labelLeft="Email" 
+    placeholder="@gmail.com" 
+  />
+  <Spacer/>
+  <Input 
+    underlined 
+    labelLeft="Password" 
+    placeholder="****" 
+  />
+    </Card.Body>
+    <Card.Divider />
+    <Card.Footer>
+      <Row justify="flex-end">
+        <Button size="sm"  onClick={()=>{
+            change(true)
+        }} light>
+          Login
+        </Button>
+        <Button size="sm" color="secondary">Signup</Button>
+      </Row>
+    </Card.Footer>
+  </Card>
+}
